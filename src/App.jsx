99f95396285044955397a1525c29c4992eae9c88
Harshit Behl <harshit.behl.96@gmail.com>
2021-10-18 21:41:34 -0400

    Initial App Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h2>A Simple React Template by Harshit Behl</h2>
+    </div>
+  );
+}
+
+export default App;
