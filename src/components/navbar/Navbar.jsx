2743f620af73c11a05f7c7832fbf2b4d149e975c
Harshit Behl <harshit.behl.96@gmail.com>
2021-10-20 02:54:28 -0400

    Navbar Implemented

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { NotificationsNone, Settings } from "@material-ui/icons";
+import { Avatar, Badge } from "@material-ui/core";
 import "./Navbar.scss";
 
 function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // JS Window Scroll Method, whenever we scroll it runs the function
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+  };
+
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar--scrolled" : "navbar"}>
       <div className="navbar__container">
         <div className="navbar__left">
           <div className="navbar__logo">
@@ -11,7 +20,16 @@ function Navbar() {
             <span>by Harshit Behl</span>
           </div>
         </div>
-        <div className="navbar__right">Right</div>
+        <div className="navbar__right">
+          <Badge badgeContent={4}>
+            <NotificationsNone className="navbar__notification-icon" />
+          </Badge>
+          <Settings className="navbar__settings-icon" />
+          <Avatar
+            className="navbar__avatar"
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80"
+          />
+        </div>
       </div>
     </div>
   );
