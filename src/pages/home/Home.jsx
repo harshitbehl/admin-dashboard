0229084c1ab914ab3b6e26318f056b54954b8b44
Harshit Behl <harshit.behl.96@gmail.com>
2021-10-22 22:16:38 -0400

    Chart Component Implemented

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import Chart from "../../components/chart/Chart";
+import { userData } from "../../chartData/dummyData";
 import "./Home.scss";
 
 function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        dataKey="Active User"
+        grid
+      />
     </div>
   );
 }
