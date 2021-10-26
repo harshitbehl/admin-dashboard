481cd3f6b9f76e9aa02c6637ec3a478c0a26256a
Harshit Behl <harshit.behl.96@gmail.com>
2021-10-26 01:49:27 -0400

    Home Page Widget Large Implemented

--- src/components/widgetSm/WidgetSm.jsx
+++ src/components/widgetSm/WidgetSm.jsx
@@ -8,11 +8,11 @@ function WidgetSm() {
     <div className="widgetSm">
       <h3 className="widgetSm__title">New Members</h3>
       <ul className="widgetSm__list">
-        <WidgetSmItem />
-        <WidgetSmItem />
-        <WidgetSmItem />
-        <WidgetSmItem />
-        <WidgetSmItem />
+        <WidgetSmItem username="Dana Ivanova" userTitle="Web Developer" />
+        <WidgetSmItem username="Harshit Behl" userTitle="UI/ UX Developer" />
+        <WidgetSmItem username="Jack Sparrow" userTitle="Economist" />
+        <WidgetSmItem username="Nick Fury" userTitle="Financial Analyst" />
+        <WidgetSmItem username="Peter Parker" userTitle="Project Manager" />
       </ul>
     </div>
   );
--- src/components/widgetSmItem/WidgetSmItem.jsx
+++ src/components/widgetSmItem/WidgetSmItem.jsx
@@ -3,16 +3,18 @@ import { Avatar } from "@material-ui/core";
 import { Visibility } from "@material-ui/icons";
 import "./WidgetSmItem.scss";
 
-function WidgetSmItem() {
+function WidgetSmItem({ username, userTitle }) {
   return (
     <li className="widgetSmItem">
-      <Avatar
-        className="widgetSmItem__avatar"
-        src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80"
-      />
+      <div className="widgetSmItem__avatar-container">
+        <Avatar
+          className="widgetSmItem__avatar"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80"
+        />
+      </div>
       <div className="widgetSmItem__user-container">
-        <p className="widgetSmItem__username">Dana Ivanova</p>
-        <p className="widgetSmItem__user-title">Web Developer</p>
+        <p className="widgetSmItem__username">{username}</p>
+        <p className="widgetSmItem__user-title">{userTitle}</p>
       </div>
       <button className="widgetSmItem__button">
         <Visibility />
